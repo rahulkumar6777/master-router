92923ba65af54b39004b161adca4cedee359c2e5
rahulkumar6777 <rahulk48546@gmail.com>
2026-02-25 18:44:39 +0530

    feat: add Dockerfile and configure Redis connection settings

--- src/configs/redis.js
+++ src/configs/redis.js
@@ -1,8 +1,8 @@
 import Redis from 'ioredis';
 
 const redisclient = new Redis({
-  host: process.env.REDIS_HOST || 'localhost',
-  port: process.env.REDIS_PORT || 6379,
+  host: 'redis',
+  port: 6379,
 });
 
 const redisConnect = async () => {
